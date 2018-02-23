967bf9fa27b3b69f9a28a0cecc6f03918d560c96
afrieirhamakram <32460534+afrieirhamakram@users.noreply.github.com>
2018-02-23 16:06:15 +0800

    Lab 2 Question 2

--- Answers/Lab 2/Question 2/src/question/pkg2/Question2.java
+++ Answers/Lab 2/Question 2/src/question/pkg2/Question2.java
@@ -15,7 +15,20 @@ public class Question2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
+        permuteString("", "ABC");
+
+    }
+
+    public static void permuteString(String beginningString, String endingString) {
+      if (endingString.length() <= 1)
+        System.out.println(beginningString + endingString);
+      else{
+        for (int i = 0; i < endingString.length(); i++) {
+          String newString = endingString.substring(0, i) + endingString.substring(i + 1);
+          permuteString(beginningString + endingString.charAt(i), newString);
+        }
+      }
     }
 
 }
